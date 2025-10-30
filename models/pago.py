522246c69c532d450a8aae26dd773b21e209461e
ljpabone <ljpabon@unicesar.edu.co>
2025-10-30 17:47:07 -0500

    Add Pago model with SQLAlchemy integration

--- models/pago.py
+++ models/pago.py
@@ -1,1 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+class Pago(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    monto = db.Column(db.Float, nullable=False)
